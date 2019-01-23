d2fa91c112e7d06a2e2f2a3f8dab060a3c565dbd
chenkui <chenkui@chkui.com>
2019-01-23 18:11:31 +0800

    fix bin bugs

--- dist/bin/create-app.js
+++ dist/bin/create-app.js
@@ -1,21 +1,5 @@
 #!/usr/bin/env node
 var path = require('path'),
-    fs = require('fs-extra');
-console.log('filename:' , __filename);
-console.log('root:' , process.cwd());
-console.log('dirname:',__dirname);
-
-/**
- * 创建模板项目
- * @param type {String} 项目类型 [standard|simple|with-antd]
- */
-function createApp(type = 'standard') {
-    var ssrDir = path.resolve(__dirname, '..', 'example', type);
-    var curRoot = process.cwd();
-    try{
-        fs.copySync(ssrDir, curRoot);
-    }catch (e) {
-        console.error(e);
-    }
-}
-module.exports = createApp;
+    fs = require('fs-extra'),
+    create = require('../sh/create');
+create();
